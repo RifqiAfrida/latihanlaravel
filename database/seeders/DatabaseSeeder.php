326371c877d5421661ed68478163d45be5dd3cdc
RifqiAfrida <rifqiebong2507@gmail.com>
2022-02-07 17:31:19 +0700

    Laravel - 8 (Seeder/Seeding)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +16,77 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Rifqi',
+            'email' => 'rifqiebong2507@gmail.com',
+            'password' => bcrypt('250704')
+
+        ]);
+
+        User::create([
+            'name' => 'Yarda',
+            'email' => 'yarda@gmail.com',
+            'password' => bcrypt('12345')
+
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+
+        ]);
+            
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Isi Judul Pertama',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Isi Judul Kedua',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Isi Judul ketiga',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Isi Judul keempat',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kelima',
+            'slug' => 'judul-kelima',
+            'excerpt' => 'Isi Judul kelima',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,11 +4,16 @@
 <h1 class="mb-5">Halaman BlogPost</h1>
 
 @foreach ($posts as $post)
-<article>
-    <h2>
-    <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a> 
-    </h2>
-    {{ $post->excerpt }}
+<article class="mb-5 border-bottom pb-4">
+    <h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}<a></h2>
+
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in 
+        <a href="/categories/{{ $post->category->slug }}" 
+        class="text-decoration-none">{{ $post->category->name }}</a></p>
+
+    <p>{{ $post->excerpt }}</p>
+
+    <a href="/posts/{{ $post->slug }}" class="text-decoration-none"> Read More..</a>
 </article>
 @endforeach
 
