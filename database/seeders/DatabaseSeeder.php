81d03a23c79635848678982bd69d160dc40a4044
RifqiAfrida <rifqiebong2507@gmail.com>
2022-02-08 16:29:54 +0700

    Laravel - 8 (Factory & Faker)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,21 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
 
-        User::create([
-            'name' => 'Rifqi',
-            'email' => 'rifqiebong2507@gmail.com',
-            'password' => bcrypt('250704')
+        // User::create([
+        //     'name' => 'Rifqi',
+        //     'email' => 'rifqiebong2507@gmail.com',
+        //     'password' => bcrypt('250704')
 
-        ]);
+        // ]);
 
-        User::create([
-            'name' => 'Yarda',
-            'email' => 'yarda@gmail.com',
-            'password' => bcrypt('12345')
+        // User::create([
+        //     'name' => 'Yarda',
+        //     'email' => 'yarda@gmail.com',
+        //     'password' => bcrypt('12345')
 
-        ]);
+        // ]);
+
+            User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -43,50 +45,52 @@ class DatabaseSeeder extends Seeder
 
         ]);
             
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Isi Judul Pertama',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-kedua',
-            'excerpt' => 'Isi Judul Kedua',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Isi Judul Pertama',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ketiga',
-            'slug' => 'judul-ketiga',
-            'excerpt' => 'Isi Judul ketiga',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Isi Judul Kedua',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Keempat',
-            'slug' => 'judul-keempat',
-            'excerpt' => 'Isi Judul keempat',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'slug' => 'judul-ketiga',
+        //     'excerpt' => 'Isi Judul ketiga',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Kelima',
-            'slug' => 'judul-kelima',
-            'excerpt' => 'Isi Judul kelima',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Keempat',
+        //     'slug' => 'judul-keempat',
+        //     'excerpt' => 'Isi Judul keempat',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Kelima',
+        //     'slug' => 'judul-kelima',
+        //     'excerpt' => 'Isi Judul kelima',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic a rem in ut. Repellendus delectus sunt dicta quam tenetur recusandae.',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
 
     }
 }
