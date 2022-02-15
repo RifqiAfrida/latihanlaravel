4ad739ef0fc0e7c188fe13b482ce851d1e237eeb
RifqiAfrida <rifqiebong2507@gmail.com>
2022-02-15 12:09:21 +0700

    Redesign UI

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,6 +40,12 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Category::create([
+            'name' => 'Web Desain',
+            'slug' => 'web-design'
+
+        ]);
+
+        Category::create([
             'name' => 'Personal',
             'slug' => 'personal'
 
--- routes/web.php
+++ routes/web.php
@@ -7,33 +7,23 @@ use App\Http\Controllers\PostController;
 
 use App\Models\Category;
 use App\Models\User;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/home', function () {
     return view ('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view ('about', [
         "title" => "About",
+        "active" => 'about',
         "nama" => "Muhamad Rifqi Afrida",
         "email" => "akongogle1@gmail.com",
         "image" => "image1.jpeg"
     ]);
 });
-
-
 
 Route::get('/posts', [PostController::class, 'index']);
 
@@ -42,6 +32,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function() {
     return view('categories', [
         'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -49,6 +40,7 @@ Route::get('/categories', function() {
 Route::get('/categories/{category:slug}', function (Category $category){
     return view('posts', [
         'title' => "Post By Category : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('category', 'author'),
         // 'category' => $category->name
     ]);
@@ -57,6 +49,7 @@ Route::get('/categories/{category:slug}', function (Category $category){
 Route::get('/authors/{author:username}', function(User $author){
     return view('posts', [
         'title' => "Post By Author : $author->name",
+        'active' => 'posts',
         'posts' => $author->posts->load('category', 'author'),
     ]);
 });
